9810c255bd6d99fab6fc3de0986c1b69713acfad
ArchieAtpu <91547511+ArchieAtpu@users.noreply.github.com>
2021-12-27 18:37:29 -0500

    Update menuScene.py

--- menuScene.py
+++ menuScene.py
@@ -1,12 +1,19 @@
 import pygame
 
 def menuScene(events, screen, state, globals):
-    # code for the menu goes here
-    # store persistent variables in the state
-    # store global variables in globals
-    # return False to stop the program
-    # return the name of another scene to switch scenes
+        pygame.init()
+    screen = pygame.display.set_mode((1000, 500))
+
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+            ...
     
+        screen.blit(screen, (0, 0))
+
+        pygame.display.update()
+
     for event in events:
         if event.type == pygame.QUIT:
             return False
