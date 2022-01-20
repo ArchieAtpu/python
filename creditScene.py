6bde0bc0276597e7ad69c9c1fa06f8e92ffdac26
ArchieAtpu <91547511+ArchieAtpu@users.noreply.github.com>
2022-01-19 19:42:42 -0500

    Update creditScene.py

--- creditScene.py
+++ creditScene.py
@@ -11,7 +11,7 @@ def creditScene(events, screen, state, globals):
             if mousePos[0] > 388 and mousePos[0] < 640 and mousePos[1] > 657 and mousePos[1] < 743:
                 return "credits" 
             elif mousePos[0] > 827 and mousePos[0] < 1004 and mousePos[1] > 693 and mousePos[1] < 753:
-                return "Earth/Mars"
+                return "menu"
 
     screen.blit(background, (0,0))
     print(pygame.mouse.get_pos())
